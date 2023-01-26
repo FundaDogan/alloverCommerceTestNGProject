34f3a7b96bdd789e0fa64e2a001d675aa872ba9c
Mustafa Babadagi <fathermountain@gmail.com>
2023-01-26 16:54:59 -0500

    first commit

--- src/test/java/allover_commerce/pages/VendorMyAccountPage.java
+++ src/test/java/allover_commerce/pages/VendorMyAccountPage.java
@@ -1,6 +1,8 @@
 package allover_commerce.pages;
 
 import allover_commerce.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class VendorMyAccountPage {
@@ -8,5 +10,51 @@ public class VendorMyAccountPage {
     public VendorMyAccountPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //Mustafa=============================================================================================================
+
+    @FindBy( linkText = "Store Manager") ////a[@href='https://allovercommerce.com/store-manager/']
+    public WebElement storeMenagerButton;
+
+    @FindBy( linkText = "Products")
+    public WebElement productsButton;
+
+    @FindBy( linkText = "Add New")
+    public WebElement addNewButton;
+
+    @FindBy( className = "page_collapsible_content_holder")
+    public WebElement inventoryButton;
+
+
+
+
+
+
+ //Mustafa=============================================================================================================
+
+
+
+
+
+
+
 
 }
