2213987757d3b0ab13ca9c91e2468ea26b4ad708
Hatice Erzincan Sozeri <haticeesozeri@gmail.com>
2023-01-26 15:14:12 -0500

    New message

--- src/test/java/allover_commerce/tests/HelloWorld.java
+++ src/test/java/allover_commerce/tests/HelloWorld.java
@@ -2,7 +2,9 @@ package allover_commerce.tests;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        System.out.println("Bu mesaji okuyanlar alta cevap yazsin");
     }
 
 
