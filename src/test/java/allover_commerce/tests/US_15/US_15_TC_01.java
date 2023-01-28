b9dd5258706ee897191c3d12b1bee9d4c88f6996
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-28 17:36:09 -0600

    commit

--- src/test/java/allover_commerce/tests/US_15/US_15_TC_01.java
+++ src/test/java/allover_commerce/tests/US_15/US_15_TC_01.java
@@ -1,9 +1,4 @@
 package allover_commerce.tests.US_15;
 
-import org.testng.annotations.Test;
-
 public class US_15_TC_01 {
-    public static void main(String[] args) {
-        System.out.println("deneme");
-    }
 }
---
+++ src/test/java/allover_commerce/tests/github_deneme.java
@@ -0,0 +1,5 @@
+package allover_commerce.tests;
+
+public class github_deneme {
+    //hello
+}
